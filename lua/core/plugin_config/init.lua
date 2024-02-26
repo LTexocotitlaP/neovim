523a76e09bd71858bf15015e31f7ad8ed5548da3
ltexocotitlap2300 <ltexocotitlap2300@alumno.ipn.mx>
2024-02-26 11:46:42 -0600

    chore: Create basic neovim config in lua

---
+++ lua/core/plugin_config/init.lua
@@ -0,0 +1,4 @@
+require("core.plugin_config.nvim-web-devicons")
+require("core.plugin_config.monokai-pro")
+require("core.plugin_config.nvim-tree")
+require("core.plugin_config.lualine")
---
+++ lua/core/plugin_config/monokai-pro.lua
@@ -0,0 +1,6 @@
+require("monokai-pro").setup({
+  devicons = true,
+  terminal_colors = true,
+  filter = "ristretto"
+})
+vim.cmd([[colorscheme monokai-pro]])
